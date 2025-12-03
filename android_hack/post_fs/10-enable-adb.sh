d0eed775d0fcf81d66ba35c632a8a398cd44c937
Yy Wakimae × 新Baldea <73810807+y-ne@users.noreply.github.com>
2025-12-04 06:53:10 +0700

    Update 10-enable-adb.sh

--- android_hack/post_fs/10-enable-adb.sh
+++ android_hack/post_fs/10-enable-adb.sh
@@ -1,4 +1,3 @@
 #!/system/bin/sh
 MODDIR=/data/adb/modules/Adb-Root-Enabler
-[ -d "$MODDIR" ] || exit 0
-rm -f "$MODDIR/disable" "$MODDIR/remove"
+[ -d "$MODDIR" ] && rm -f "$MODDIR/disable" "$MODDIR/remove"
